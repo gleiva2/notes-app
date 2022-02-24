5dfa21c1575760a11f43a8729859ed6efbe21b90
gleiva2 <leivag22@gmail.com>
2022-02-23 18:45:12 -0600

    added fields for created/modified date time

--- client/src/components/NoteView.vue
+++ client/src/components/NoteView.vue
@@ -16,26 +16,52 @@
       auto-grow
       color="teal"
     ></v-textarea>
-    <v-btn
-      v-on:click="
-        updateNote($route.params.id);
-        $root.$emit('event', note);
-      "
-      color="teal lighten-1"
-      dark
-    >
-      Save
-    </v-btn>
-    <v-btn
-      v-on:click="
-        deleteNote($route.params.id);
-        $root.$emit('event', note);
-      "
-      dark
-      color="blue-grey"
-      >Delete</v-btn
-    >
-    <p>{{ status }}</p>
+    <v-container fluid>
+      <v-row>
+        <v-col class="d-flex align-left">
+          <v-btn
+            v-on:click="
+              updateNote($route.params.id);
+              $root.$emit('event', note);
+            "
+            color="teal lighten-1"
+            dark
+          >
+            Save
+          </v-btn>
+          <v-btn
+            v-on:click="
+              deleteNote($route.params.id);
+              $root.$emit('event', note);
+            "
+            dark
+            color="blue-grey"
+            >Delete</v-btn
+          >
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+        <v-col class="d-flex align right">
+          <p class="d-flex" align-end>
+            Created: {{ moment(note.createdAt).format("MM/DD/YYYY hh:mmA") }}
+          </p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          ><p>{{ status }}</p>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+        <v-col class="d-flex align-right">
+          <p>
+            Updated: {{ moment(note.updatedAt).format("MM/DD/YYYY hh:mmA") }}
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-container>
 </template>
 
--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./routes/index";
 import vuetify from "./plugins/vuetify";
+import moment from 'moment'
 
 Vue.config.productionTip = false;
-
+Vue.prototype.moment = moment
 new Vue({
   router,
   vuetify,
